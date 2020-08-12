5daa5437c0d75d0f1e1cdcb9b8b908eecd99787c
Mayank Hotchandani <mayank182v@gmail.com>
2020-08-12 16:40:49 +0530

    Done

--- index.js
+++ index.js
@@ -20,3 +20,16 @@ searchUsers.addEventListener("keyup", function () {
 $(".search").on("click", function () {
   $(".search-users").toggle("display: inline-block");
 });
+
+//Video play
+$(".video")
+  .parent()
+  .click(function () {
+    if ($(this).children(".video").get(0).paused) {
+      $(this).children(".video").get(0).play();
+      $(this).children(".playpause").fadeOut();
+    } else {
+      $(this).children(".video").get(0).pause();
+      $(this).children(".playpause").fadeIn();
+    }
+  });
